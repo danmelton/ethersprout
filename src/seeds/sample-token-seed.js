35eaf1534a875aa910f841a883437b119fc7a4d5
Dan Melton <melton.dan@gmail.com>
2018-04-26 10:26:07 -0700

    adding sample seed command

--- src/seeds/sample-token-seed.js
+++ src/seeds/sample-token-seed.js
@@ -3,8 +3,10 @@ const SampleToken = artifacts.require('../contracts/SampleToken.sol')
 // use console.log(this) to see top level JS object
 // const accounts = web3.eth.accounts
 const runMe = async () => {
+  const accounts = web3.eth.accounts
   const tokenContract = await SampleToken.deployed()
-  console.log(tokenContract.networks)
+  console.log(accounts)
+  console.log(tokenContract)
 }
 
 module.exports = async () => {
